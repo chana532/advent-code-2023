473a4d945bf223f878710d503496f835094a7ce5
Anna <chana5320@gmail.com>
2023-12-05 17:59:03 -0500

    empty day 5 file

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # advent-code-2023
 
-Written by Anna using typescript 
+Written by Anna using Typescript and no brain cells :D
+
+Day 1: complete head empty though
+Day 2: complete and to lazy to clean up
+Day 3: skipped i hate these types of problems
+Day 4: complete and a somewhat ok solution
+Day 5: skip to many words 
+Day 6: 
+Day 7: 
---
+++ days/day5.ts
@@ -0,0 +1,32 @@
+import { readFile } from '../util/readFile';
+
+
+const solutionPartOne = (lines: any) => {
+    for(var line of lines) {
+
+    }
+
+    console.log('solutionPartOne: ')
+}
+
+
+
+const solutionPartTwo = (lines: any) => {
+    for(var line of lines) {
+
+    }
+
+    console.log('solutionPartTwo: ')
+}
+
+
+
+
+(async () => {
+    const linesExample = (await readFile('./inputs/example')).split('\r\n');
+    solutionPartOne(linesExample)
+    solutionPartTwo(linesExample)
+    const lines = (await readFile('./inputs/day5')).split('\r\n');
+    solutionPartOne(lines)
+    solutionPartTwo(lines)
+})();;
